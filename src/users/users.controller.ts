01908e09b8b29408b9cc84bc380811de72e5ac68
fitrah-firdaus <fitrah.firdaus@gmail.com>
2024-11-19 12:49:08 +0700

    First Commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './users.interface';
+
+@Controller('api/users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  findAll(): User[] {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): User | undefined {
+    return this.usersService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() user: Partial<User>): User {
+    return this.usersService.create(user);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updatedUser: Partial<User>): User | undefined {
+    return this.usersService.update(id, updatedUser);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string): User | undefined {
+    return this.usersService.delete(id);
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { User } from './users.interface';
+import { v4 as uuid } from 'uuid'; // For generating unique IDs
+
+@Injectable()
+export class UsersService {
+  private users: User[] = [];
+
+  findAll(): User[] {
+    return this.users;
+  }
+
+  findOne(id: string): User | undefined {
+    return this.users.find(user => user.id === id);
+  }
+
+  create(user: Partial<User>): User {
+    const newUser: User = { id: uuid(), ...user } as User;
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  update(id: string, updatedUser: Partial<User>): User | undefined {
+    const index = this.users.findIndex(user => user.id === id);
+    if (index === -1) return undefined;
+    this.users[index] = { ...this.users[index], ...updatedUser };
+    return this.users[index];
+  }
+
+  delete(id: string): User | undefined {
+    const index = this.users.findIndex(user => user.id === id);
+    if (index === -1) return undefined;
+    const [deletedUser] = this.users.splice(index, 1);
+    return deletedUser;
+  }
+}
